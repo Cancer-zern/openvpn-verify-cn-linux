873ef2545c00826fe143001ca2f3b68a143fb71a
Cancer-zern <68541237+Cancer-zern@users.noreply.github.com>
2022-06-02 01:14:25 +0400

    Update verify-cn.sh

--- verify-cn.sh
+++ verify-cn.sh
@@ -8,7 +8,7 @@ if [ $2 -eq 0 ] ; then
 # $3 -> X509_NAME_oneline
 # $1 -> cn we are looking for
 grep -q "^`expr match "$3" ".*CN=\([^,]*\)"`$" "$1" && exit 0
-#echo "*** W A R N I N G *** User \""`expr match "$3" ‘.*CN=\([^,]*\)’`"\" not in whitelist ("$1")!" && exit 1
+#echo "*** W A R N I N G *** User \""`expr match "$3" '.*CN=\([^,]*\)'`"\" not in whitelist ("$1")!" && exit 1
 exit 1
 fi
 
